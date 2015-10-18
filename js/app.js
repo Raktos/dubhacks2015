c5a7e5ca264dafab6b6818ea988c04ea4cfd9438
Jason Ho <raktos@uw.edu>
2015-10-17 23:46:50 -0700

    firebase connection

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@
 var app = angular.module('uniApp', []);
 
 app.controller("MainCtrl", function($scope) {
+    $scope.firebase = new Firebase('https://uni-app.firebaseio.com/');
     $scope.email;
     $scope.password;
 
 
-    // perform a get request to authenticate the user 
+
+    // perform a get request to authenticate the user
     $scope.login = function() {
 
-    }
+    };
 });
